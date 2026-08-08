6eb2de491a325d67ccb4a177d97677209ca8537b
robnyg1984 <robnyg1984@gmail.com>
2026-08-08 20:08:46 +0200

    Multiplayer work in progress

---
+++ Module2/Assets/MultiPlayer/Scripts/AstarPathfinding.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public class AstarPathfinding
+    {
+        private const float InitialCost = float.PositiveInfinity;
+        private Grid grid;
+
+        public AstarPathfinding(Grid grid)
+        {
+            this.grid = grid;
+        }
+
+        public List<Node> FindPath(Vector2Int start, Vector2Int goal)
+        {
+            ResetNodes();
+
+            var startNode = grid.nodeArray[start.x, start.y];
+            var goalNode = grid.nodeArray[goal.x, goal.y];
+
+            var open = new List<Node>();
+            var closed = new HashSet<Node>();
+
+            startNode.G = 0;
+            startNode.H = Heuristic(startNode, goalNode);
+            open.Add(startNode);
+
+            while (open.Count > 0)
+            {
+                Node current = GetLowestF(open);
+
+                if (current == goalNode)
+                    return ReconstructPath(current);
+
+                open.Remove(current);
+                closed.Add(current);
+
+                foreach (var neighbor in grid.GetNeighbors(current))
+                {
+                    if (!neighbor.walkable || closed.Contains(neighbor))
+                        continue;
+
+                    float tentativeG = current.G + 1f;
+
+                    if (tentativeG < neighbor.G)
+                    {
+                        neighbor.Parent = current;
+                        neighbor.G = tentativeG;
+                        neighbor.H = Heuristic(neighbor, goalNode);
+
+                        if (!open.Contains(neighbor))
+                            open.Add(neighbor);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private float Heuristic(Node a, Node b)
+        {
+            return Mathf.Abs(a.gridXPos - b.gridXPos) + Mathf.Abs(a.gridYPos - b.gridYPos);
+        }
+
+        /// <summary>
+        /// Noden med lägsta kostnad som inte är en sten.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private Node GetLowestF(List<Node> list)
+        {
+            Node best = list[0];
+            float bestF = best.F;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].F < bestF)
+                {
+                    best = list[i];
+                    bestF = list[i].F;
+                }
+            }
+
+            return best;
+        }
+
+        private List<Node> ReconstructPath(Node end)
+        {
+            var path = new List<Node>();
+            var current = end;
+            int maxLength = grid.nodeArray.Length;
+
+            while (current != null && path.Count <= maxLength)
+            {
+                path.Add(current);
+                current = current.Parent;
+            }
+
+            path.Reverse();
+            return path;
+        }
+        
+        private void ResetNodes()
+        {
+            foreach (var node in grid.nodeArray)
+            {
+                node.G = InitialCost;
+                node.H = 0f;
+                node.Parent = null;
+            }
+        }
+    }
+}
---
+++ Module2/Assets/MultiPlayer/Scripts/Grid.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Multiplayer
+{
+    public class Grid : MonoBehaviour
+    {
+        public Vector2 gridWorldSize;
+        public GameObject tilePrefab;
+
+        [Min(1)] public int grassAmount;
+        [Min(1)] public int stoneAmount;
+        public GameObject sheepGameObject;
+
+        public static Grid instance;
+        public float tileScale = 10;
+        public int minTileSize = 1;
+        public int maxTileSize = 3;
+
+        public Node[,] nodeArray;
+        public float nodeDiameter;
+
+        private Sheep currentSheep = null;
+
+        private void OnEnable()
+        {
+            instance = this;
+        }
+
+        private void OnDisable()
+        {
+            instance = null;
+        }
+
+        void Start()
+        {
+            CreateGrid();
+        }
+
+
+        void Update()
+        {
+            /*
+             a. Om det finns mer än 2 gräs tiles i spelvärlden då ska får prefaben
+                spawnas!
+             */
+            if (currentSheep == null)
+            {
+                int currentGrassTilesCount = 0;
+                Node targetNode = null;
+                foreach (var node in nodeArray)
+                {
+                    if (node.tileObject.tileType == TileType.Grass)
+                    {
+                        currentGrassTilesCount++;
+                        targetNode = node;
+                    }
+
+                    if (currentGrassTilesCount > 2)
+                    {
+                        break;
+                    }
+                }
+
+                if (currentGrassTilesCount > 2 && targetNode != null)
+                {
+                    currentSheep = Instantiate(sheepGameObject, targetNode.worldPosition, Quaternion.identity)
+                        .GetComponent<Sheep>();
+                    currentSheep.OnSheepNotHungry.AddListener(SheepNotHungry);
+                    currentSheep.SetCurrentNode(targetNode);
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        }
+
+        public void SheepNotHungry()
+        {
+            Destroy(currentSheep.gameObject);
+        }
+
+        public void CreateGrid()
+        {
+            int gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+            int gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+            if (nodeArray != null && nodeArray.Length > 0)
+            {
+                // Töm först....
+                for (int x = 0; x < gridSizeX; x++)
+                {
+                    for (int y = 0; y < gridSizeY; y++)
+                    {
+                        nodeArray[x, y] = null;
+                    }
+                }
+            }
+
+            nodeArray = new Node[gridSizeX, gridSizeY];
+            Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 -
+                                 Vector3.forward * gridWorldSize.y / 2;
+
+            int numberOfTiles = gridSizeX * gridSizeY;
+            int tilesGrassDrawn = 0, tilesStoneDrawn = 0;
+
+            // Jord
+            DrawDirt(gridSizeX, gridSizeY, bottomLeft);
+
+            // Gräs
+            tilesGrassDrawn = DrawTilesRandom(gridSizeX, gridSizeY, bottomLeft, grassAmount, TileType.Grass);
+
+            // Sten
+            tilesStoneDrawn = DrawTilesRandom(gridSizeX, gridSizeY, bottomLeft, stoneAmount, TileType.Stone);
+        }
+
+        public IEnumerable<Node> GetNeighbors(Node node)
+        {
+            int gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+            int gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+            int[,] dirs =
+            {
+                { 1, 0 },
+                { -1, 0 },
+                { 0, 1 },
+                { 0, -1 }
+            };
+
+            for (int i = 0; i < dirs.GetLength(0); i++)
+            {
+                int nx = node.gridXPos + dirs[i, 0];
+                int ny = node.gridYPos + dirs[i, 1];
+
+                if (nx >= 0 && ny >= 0 && nx < gridSizeX && ny < gridSizeY)
+                {
+                    var n = nodeArray[nx, ny];
+                    if (n.walkable)
+                        yield return n;
+                }
+            }
+        }
+
+        private int DrawTilesRandom(int gridSizeX, int gridSizeY, Vector3 bottomLeft, int amount, TileType tileType)
+        {
+            int tilesCount = 0;
+            int tilesDrawn = 0;
+            while (tilesCount < amount)
+            {
+                int x = Random.Range(0, gridSizeX);
+                int y = Random.Range(0, gridSizeY);
+
+                if (nodeArray[x, y] != null && (nodeArray[x, y].tileObject.tileType != TileType.Dirt))
+                {
+                    tilesCount++;
+                    continue;
+                }
+
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeDiameter / 2) +
+                                     Vector3.forward * (y * nodeDiameter + nodeDiameter / 2);
+
+                Tile tile = Instantiate(tilePrefab, worldPoint, Quaternion.identity).GetComponent<Tile>();
+                tile.transform.parent = transform;
+                nodeArray[x, y] = new Node(worldPoint, tile, tileType, x, y);
+                tilesCount++;
+                tilesDrawn++;
+            }
+
+            return tilesDrawn;
+        }
+
+        private void DrawDirt(int gridSizeX, int gridSizeY, Vector3 bottomLeft)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                for (int y = 0; y < gridSizeY; y++)
+                {
+                    if (nodeArray[x, y] != null)
+                    {
+                        continue;
+                    }
+
+                    Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeDiameter / 2) +
+                                         Vector3.forward * (y * nodeDiameter + nodeDiameter / 2);
+
+                    Tile tileDirt = Instantiate(tilePrefab, worldPoint, Quaternion.identity).GetComponent<Tile>();
+                    tileDirt.transform.parent = transform;
+                    nodeArray[x, y] = new Node(worldPoint, tileDirt, TileType.Dirt, x, y);
+                }
+            }
+        }
+    }
+}
